2f01b5d20b2c09b825a256ff6245ff892b39f4f3
yifeil <yifeil@vmware.com>
2018-12-03 14:28:48 +0800

    Add Flask proj

---
+++ Foundation/app/server.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__, static_folder='/Users/yifeil/git/PersonalWebsite/Foundation/assets',
+            template_folder='../templates')
+
+
+@app.route('/hello')
+def hello_world():
+    return '吼吼吼田小桐'
+
+
+@app.route('/y')
+def trick():
+    return render_template('trick.html')
+
+
+@app.route('/')
+def index():
+    return render_template('statistic.html')
+
+
+app.run(host='0.0.0.0')
